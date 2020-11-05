03a4d3b36232e6723bfde652832584173cc17a7c
jim <leeyongwang@gmail.com>
2020-11-05 21:43:30 +0800

    Initial commit

---
+++ src/main/java/com/jim/swg/SwgApplication.java
@@ -0,0 +1,13 @@
+package com.jim.swg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jim/swg/SwgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jim.swg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
